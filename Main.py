282a94e157f1f94a10d1b9cd25e3ee77985dd57b
marc <marc.georges.turk@estudiantat.upc.edu>
2024-06-05 17:41:52 +0200

    revisió 2

--- Main.py
+++ Main.py
@@ -1,12 +1,12 @@
 from PyQt5.QtWidgets import QApplication
-from View import MainView
+from View import MainView  # Importem la classe MainView des del fitxer View.py
 from Presenter import MainPresenter
 from Model import TemperatureModel
 
 def main():
     app = QApplication([])
     model = TemperatureModel('temperatures_habitacions.xlsx')
-    view = MainView()
+    view = MainView()  # Creem una instància de MainView
     presenter = MainPresenter(view)
     view.show()
     app.exec_()
