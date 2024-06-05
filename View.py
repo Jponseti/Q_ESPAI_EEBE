ac0e729cf5bce2e8770945ec1edbfa980e74d4a6
marc <marc.georges.turk@estudiantat.upc.edu>
2024-06-05 17:57:01 +0200

    revisió 2

--- View.py
+++ View.py
@@ -1,3 +1,7 @@
+from PyQt5.QtWidgets import QMainWindow, QComboBox
+from ui_EEBE_Mapa import Ui_Planta_3
+from Model import TemperatureModel
+
 class MainView(QMainWindow, Ui_Planta_3):
     def __init__(self):
         super(MainView, self).__init__()
@@ -7,3 +11,21 @@ class MainView(QMainWindow, Ui_Planta_3):
         self.dia.addItems([str(i) for i in range(1, 31)])
         self.mes.addItems([str(i) for i in range(1, 13)])
         self.any.addItems(['2024'])
+
+        self.dia.currentIndexChanged.connect(self.actualitzar_temperatures)
+        self.mes.currentIndexChanged.connect(self.actualitzar_temperatures)
+        self.any.currentIndexChanged.connect(self.actualitzar_temperatures)
+
+    def actualitzar_temperatures(self):
+        if self.dia.currentText() == '-' or self.mes.currentText() == '-' or self.any.currentText() == '-':
+            return
+
+        day = int(self.dia.currentText())
+        month = int(self.mes.currentText())
+        year = int(self.any.currentText())
+
+        for i in range(1, 19):
+            room = f'H{i}'
+            temperature = self.model.get_temperature(day, month, year, room)
+            label = getattr(self, f'A{i}')
+            label.setText(f"{temperature}°C")
