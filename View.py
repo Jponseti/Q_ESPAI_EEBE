5a2d8c2d23e6378d90f97ad401a3e4bf434df03b
marc <marc.georges.turk@estudiantat.upc.edu>
2024-06-05 17:55:03 +0200

    revisió 2

--- View.py
+++ View.py
@@ -1,8 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow, QLabel, QComboBox
-from ui_EEBE_Mapa import Ui_Planta_3
-from Model import TemperatureModel
-
-class MainView(QMainWindow, Ui_Planta_3):  # Canviem el nom de la classe a 'MainView'
+class MainView(QMainWindow, Ui_Planta_3):
     def __init__(self):
         super(MainView, self).__init__()
         self.setupUi(self)
@@ -11,21 +7,3 @@ class MainView(QMainWindow, Ui_Planta_3):  # Canviem el nom de la classe a 'Main
         self.dia.addItems([str(i) for i in range(1, 31)])
         self.mes.addItems([str(i) for i in range(1, 13)])
         self.any.addItems(['2024'])
-
-        self.dia.currentIndexChanged.connect(self.actualitzar_temperatures)
-        self.mes.currentIndexChanged.connect(self.actualitzar_temperatures)
-        self.any.currentIndexChanged.connect(self.actualitzar_temperatures)
-
-    def actualitzar_temperatures(self):
-        if self.dia.currentText() == '-' or self.mes.currentText() == '-' or self.any.currentText() == '-':
-            return
-
-        day = int(self.dia.currentText())
-        month = int(self.mes.currentText())
-        year = int(self.any.currentText())
-
-        for i in range(1, 19):  # Ajustament de l'índex superior a 19
-            room = f'H{i}'
-            temperature = self.model.get_temperature(day, month, year, room)
-            label = getattr(self, f'A{i}')
-            label.setText(f"{temperature}°C")
