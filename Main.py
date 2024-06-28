ef0871288debbe9ee30747e8b3d3c2daf1a9901f
Anna Garcia <anna.garcia.picazo@estudiantat.upc.edu>
2024-06-28 09:39:01 +0200

    resolucio tret

--- Main.py
+++ Main.py
@@ -8,13 +8,7 @@ from Presenter import Presenter
 from Model import Model
 
 def main():
-    os.environ["QT_SCALE_FACTOR"] = "1.2"  # Variable de entorno para escalar
     app = QtWidgets.QApplication(sys.argv)
-
-    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True) #Habilita el escalado automático de la interfaz de usuario para pantallas de alta DPI
-    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True) #Asegura que los pixmaps (imágenes) se escalen correctamente en pantallas de alta DPI.
-
-
     model = Model()
     view = View()
     presenter = Presenter(view, model)
